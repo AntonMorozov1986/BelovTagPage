604946ca16a2d1d55c51ff5a85b9772ba644baf3
Anton <amorozov86@gmail.com>
2021-05-26 16:12:55 +0300

    Изменен JSON

---
+++ js/constants.js
@@ -0,0 +1,4 @@
+const URL = 'https://raw.githubusercontent.com/AntonMorozov1986/BelovTagPage/rewrite_in_OOP/httpTestRequest';
+const URL_TAGS_LIST = '/tagsRequest.json'
+
+const TAG_LIST_CSS_SELECTOR = '.tags-list'
--- js/constnts.js
+++
@@ -1,2 +0,0 @@
-const URL = 'https://raw.githubusercontent.com/AntonMorozov1986/BelovTagPage/rewrite_in_OOP/httpTestRequest';
-const URL_TAGS_LIST = '/tagsRequest.json'
